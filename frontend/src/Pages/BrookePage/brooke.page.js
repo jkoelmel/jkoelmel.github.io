fd7110c375d77b630f5fe447ea3ccadbed47499a
blporter <brookeporter13@gmail.com>
2020-09-11 16:41:22 -0700

    Add personal page for M0
    I added the .idea folder to the gitignore to get rid of IDE junk, and added relevant image, styles, and route to have my page display.

---
+++ frontend/src/Pages/BrookePage/brooke.page.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import portrait from '../../Assets/Images/brooke_portrait.jpg'
+
+class BrookePage extends Component {
+
+    render() {
+        return (
+            <div className={'brooke-page'}>
+                <img src={portrait} alt={'Portrait of Brooke Porter'} className={'brooke-image'}/>
+                <div className={'brooke-body'}>
+                    <h1>Brooke Porter</h1>
+                    <h2>Team 2: Backend Engineer</h2>
+
+                    <p>Email: bporter3@mail.sfsu.edu</p>
+                    <p>Discord: bporter#3019</p>
+                    <p>Github: <a href={"https://github.com/blporter"}>https://github.com/blporter</a></p>
+                    <p>LinkedIn: <a href={"https://www.linkedin.com/in/blp/"}>https://www.linkedin.com/in/blp/</a></p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BrookePage
