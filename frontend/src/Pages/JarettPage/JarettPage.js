8457ae446760f140c30aa785dc6bbf1701f2eb9b
Jarett Koelmel <Jarett.koelmel@gmail.com>
2020-09-13 17:30:31 -0700

    adjusted formatting for pages

--- frontend/src/Pages/JarettPage/JarettPage.js
+++ frontend/src/Pages/JarettPage/JarettPage.js
@@ -10,14 +10,14 @@ class JarettPage extends Component {
                 <img src={portrait} alt={'Jarett Koelmel'} className={'image'}/>
                 <div className={'body'}>
                     <Button href="/">Back</Button><br/><br/>
-                <h1>Jarett Koelmel</h1><br />
-                <h3>Team Lead/Scrum Master</h3><br />
+                    <h1>Jarett Koelmel</h1><br/>
+                    <h3>Team Lead/Scrum Master</h3><br/>
 
-                <p>As team leader, I am responsible for general DevOps and product deployment management.
-                    The natural overlap between leading a team and managing communication, morale, and scheduling resulted
-                    in my taking over the position of scrum master as well. While the development of tech-based products is new to my repertoire, I have a decade of
-                    experience in naval engineering, product line management, and scheduling of multiple platforms'
-                    equipment.</p>
+                    <p>As team leader, I am responsible for general DevOps and product deployment management.
+                        The natural overlap between leading a team and managing communication, morale, and scheduling
+                        resulted in my taking over the position of scrum master as well. While the development of tech-based
+                        products is new to my repertoire, I have a decade of experience in naval engineering, product line management, and scheduling of multiple platforms'
+                        equipment.</p>
                 </div>
             </div>
         );
--- frontend/src/Pages/PeterPage/PeterPage.js
+++ frontend/src/Pages/PeterPage/PeterPage.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-// import portrait from '../../Assets/Images/Paul.jpg'
 import {Button} from "react-bootstrap";
 
 class PeterPage extends Component {
@@ -7,15 +6,12 @@ class PeterPage extends Component {
     render() {
         return (
             <div>
-                {/*<img src={portrait} alt={'Paul Borst'} className={'image'}/>*/}
-                <div className={'body'}>
-                    <Button href="/">Back</Button><br/><br/>
-                        <h1>Peter Hu</h1>
-                        <h2>Team2 Back-end Lead</h2>
-                        <h3>Email: chu3@mail.sfsu.edu</h3>
-                        <p>I like jokes and riddles.</p>
-                        <p><a href="https://www.linkedin.com/in/peter-hu-0279211a2/">LinkedIn </a></p>
-                </div>
+                <Button href="/">Back</Button><br/><br/>
+                <h1>Peter Hu</h1>
+                <h2>Team2 Back-end Lead</h2>
+                <h3>Email: chu3@mail.sfsu.edu</h3>
+                <p>I like jokes and riddles.</p>
+                <p><a href="https://www.linkedin.com/in/peter-hu-0279211a2/">LinkedIn </a></p>
             </div>
         );
     }
