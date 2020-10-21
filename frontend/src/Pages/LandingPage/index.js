9537c2d7e15d5a3019300367c7c82e231ed25615
Mjcanson <mcanson@mail.sfsu.edu>
2020-10-20 22:42:23 -0700

    added react-window for scrollable list... working progress on the implementation.

--- frontend/src/Pages/LandingPage/index.js
+++ frontend/src/Pages/LandingPage/index.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import "./styles.css"
 import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+import PatientList from '../../Components/PatientList/PatientList'
 
 const Landing = () => {
+
     return (
-        <p>hello world</p>
+        <div className = 'root'>
+        <Grid container className ='grid-root' spacing={0}>
+            <Grid item xs={3} > 
+            <Paper >
+                <PatientList/>
+                </Paper>
+            </Grid>
+           
+        </Grid>
+        </div>
     )
+       
 }
 
 export default Landing
