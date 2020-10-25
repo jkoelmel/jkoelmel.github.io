ab5b9aba9670f29569699a9430784ed149f1bf62
Paul Borst <pborst@mail.sfsu.edu>
2020-10-25 16:25:47 -0700

    Update

--- frontend/src/Pages/LandingPage/index.js
+++ frontend/src/Pages/LandingPage/index.js
@@ -8,8 +8,21 @@ import PatientList from '../../Components/PatientList/PatientList'
 import SearchPlan from '../../Components/SearchPlan/SearchPlan'
 import SearchReport from '../../Components/SearchReport/SearchReport'
 import SearchActivities from '../../Components/SearchActivities/SearchActivities'
+import axios from 'axios'
 
 const Landing = () => {
+    axios.get('api/pt/patients',{
+        params: {
+            pt_id: 1
+        }
+    })
+        .then(function (response) {
+            console.log(response);
+            console.log(response.data[0].f_name);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     return (
         <div className = 'root'>
             <Grid container className ='grid-root' spacing={5}>
