d12efa7efb327577e01fd1ca6ff4119e1becc919
echen22 <echen8@mail.sfsu.edu>
2020-10-27 11:27:00 -0700

    Finished Tag,TagUtil,TaggedWith and TaggedWithUtil.

--- Back-End/SparkServer/src/main/server/Tag/Tag.java
+++ Back-End/SparkServer/src/main/server/Tag/Tag.java
@@ -1,13 +1,86 @@
 package main.server.Tag;
 
+import main.server.Server;
+
+import java.sql.*;
+
 public class Tag {
 
-    private Integer tagId;
+    private Integer tag_id;
     private String title;
 
-    public Integer getTagId(){return tagId;}
+    public Tag(Integer tag_id, String title){
+        this.tag_id=tag_id;
+        this.title=title;
+    }
 
-    public void setTagId(Integer tagId){this.tagId=tagId;}
+    public Tag(Integer tag_id){
+        this.tag_id=tag_id;
+    }
+
+    public void createTag() throws Exception{
+        String tagQuery = "INSERT INTO tag(tag_id,title) VALUES (?,?)";
+
+        try (Connection con = DriverManager.getConnection(
+                Server.databasePath,
+                Server.databaseUsername,
+                Server.databasePassword);
+             PreparedStatement pst = con.prepareStatement(tagQuery)) {
+
+            pst.setInt(1,getTagId());
+            pst.setString(2,getTitle());
+
+            pst.executeUpdate();
+
+            pst.executeUpdate(tagQuery);
+            System.out.println("Tag added to database");
+
+        }catch(SQLException ex){
+            throw new Exception("Error creating tag: " + ex.toString());
+        }
+    }
+
+    public Tag getTag() throws Exception{
+        String tagQuery = "SELECT * FROM tag";
+        try (Connection con = DriverManager.getConnection(
+                Server.databasePath,
+                Server.databaseUsername,
+                Server.databasePassword);
+             PreparedStatement pst = con.prepareStatement(tagQuery)) {
+            pst.executeQuery(tagQuery);
+
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+
+                setTagId(rs.getInt("tag_id"));
+                setTitle(rs.getString("title"));
+
+            }
+        } catch (SQLException ex) {
+            throw new Exception("Error getting tag with id " + this.tag_id + ": " + ex.toString());
+        }
+       return this;
+    }
+
+    public void updateTag(String title) throws Exception{
+        String query = "UPDATE tag SET title = " + title + " WHERE tag_id = " + this.tag_id;
+
+        try (Connection con = DriverManager.getConnection(
+                Server.databasePath,
+                Server.databaseUsername,
+                Server.databasePassword);
+             PreparedStatement pst = con.prepareStatement(query)) {
+            pst.executeUpdate(query);
+            System.out.println("Tag updated");
+        } catch (Exception ex) {
+            throw new Exception("Error updating workouts for patient with id " + this.tag_id + ": " + ex.toString());
+        }
+    }
+
+
+    public Integer getTagId(){return tag_id;}
+
+    public void setTagId(Integer tag_id){this.tag_id=tag_id;}
 
     public String getTitle(){return title;}
 
--- Back-End/SparkServer/src/main/server/Tag/TagUtil.java
+++ Back-End/SparkServer/src/main/server/Tag/TagUtil.java
@@ -1,4 +1,83 @@
 package main.server.Tag;
 
-public class TagUtil {
+import com.google.gson.Gson;
+import main.server.Server;
+import spark.Request;
+import spark.Response;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class TagUtil{
+
+    public static String selectSpecific(Request request, Response response) {
+        String toReturn = "";
+        try {
+            Tag tag = new Tag(Integer.parseInt(request.queryMap().get("tag_id").value()));
+            Gson gson = new Gson();
+            toReturn = gson.toJson(tag.getTag());
+
+            System.out.println("Tag has been selected");
+            response.type("application/json");
+            response.status(200);
+        } catch (SQLException sqlEx) {
+            System.err.println(sqlEx.toString());
+            response.status(500);
+        } catch (Exception ex) {
+            System.err.println(ex.toString());
+            response.status(400);
+        }
+        return toReturn;
+    }
+    public static String selectAll(Response response) {
+        String toReturn = "";
+
+        String query = "SELECT * FROM tag";
+
+        try (Connection con = DriverManager.getConnection(
+                Server.databasePath,
+                Server.databaseUsername,
+                Server.databasePassword);
+             PreparedStatement pst = con.prepareStatement(query)) {
+            ResultSet rs = pst.executeQuery();
+
+            ArrayList<Tag> list = new ArrayList<>();
+            while (rs.next()) {
+                Tag tag = new Tag(rs.getInt("tag_id"),rs.getString("title"));
+
+                tag.setTagId(rs.getInt("tag_id"));
+                tag.setTitle(rs.getString("title"));
+
+            }
+            Gson gson = new Gson();
+            toReturn = gson.toJson(list);
+
+            System.out.println("All tags have been selected");
+            response.type("application/json");
+            response.status(200);
+        } catch (SQLException sqlEx) {
+            System.err.println(sqlEx.toString());
+            response.status(500);
+        } catch (Exception ex) {
+            System.err.println(ex.toString());
+            response.status(400);
+        }
+        return toReturn;
+    }
+
+    public static Integer registerTag(Request request) {
+        try {
+            Tag tag = new Tag((Integer.parseInt(request.queryMap().get("tag_id").value())),
+                    request.queryMap().get("title").value());
+            tag.createTag();
+            return 200;
+        } catch (SQLException sqlEx) {
+            System.err.println(sqlEx.toString());
+            return 500;
+        } catch (Exception ex) {
+            System.err.println(ex.toString());
+            return 400;
+        }
+    }
+
 }
