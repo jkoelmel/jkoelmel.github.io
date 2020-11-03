78a1a59fc6ca5b66cd4249c7c0a21624a6dab434
Jarett Koelmel <jarett.koelmel@gmail.com>
2020-11-02 19:04:29 -0800

    Tests

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import axios from 'axios';
-
-axios.defaults.baseURL = 'https://api.pthealth.club';
 
 ReactDOM.render(<App />, document.getElementById('root')
 );
