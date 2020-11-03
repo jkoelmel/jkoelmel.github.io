72de6f4fb4a97112d55def94c37b59f6deebfffe
Jarett Koelmel <jarett.koelmel@gmail.com>
2020-11-02 18:49:50 -0800

    Tests

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,26 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.pthealth.club';
-axios.defaults.headers.common['Authorizaiton'] = 'AUTH TOKEN';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-
-axios.interceptors.request.use(request => {
-    console.log(request);
-    return request;
-}, error => {
-   console.log(error);
-    return Promise.reject(error);
-});
-
-axios.interceptors.response.use(response => {
-    console.log(response);
-    // Edit response config
-    return response;
-}, error => {
-    console.log(error);
-    return Promise.reject(error);
-});
+axios.defaults.baseURL = 'https//:api.pthealth.club';
 
 ReactDOM.render(<App />, document.getElementById('root')
 );
