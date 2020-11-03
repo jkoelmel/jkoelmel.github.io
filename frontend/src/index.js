783e7389e7a6e78cd5c87b8461845a820ce678ee
Jarett Koelmel <jarett.koelmel@gmail.com>
2020-11-02 18:50:14 -0800

    Tests

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https//:api.pthealth.club';
+axios.defaults.baseURL = 'https://api.pthealth.club';
 
 ReactDOM.render(<App />, document.getElementById('root')
 );
