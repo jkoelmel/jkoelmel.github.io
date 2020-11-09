bb860fe10539eb5dd78eee6a8c71295aefde974d
blporter <brookeporter13@gmail.com>
2020-11-08 19:49:32 -0800

    Add Redux into PatientList

--- frontend/src/Redux/store/store.js
+++ frontend/src/Redux/store/store.js
@@ -1,7 +1,13 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
 import rootReducer from "../reducer/rootReducer";
 
+// redux-thunk middleware is required for actions to work as async
+const middleware = applyMiddleware(
+    thunk,
+);
+
 //Where all the data (or states) is gonna be stored
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, middleware);
 
 export default store;
