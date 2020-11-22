a157e39e420d1c8ea6fd28fabffdb40e458fb318
blporter <brookeporter13@gmail.com>
2020-11-22 12:34:26 -0800

    Update messaging to use email

--- frontend/src/Components/Messaging/Messaging.js
+++ frontend/src/Components/Messaging/Messaging.js
@@ -1,61 +1,77 @@
-import React, { useEffect } from "react";
-import {Launcher} from 'react-chat-window'
+import React, {useEffect} from "react";
+import {
+    addResponseMessage,
+    addUserMessage,
+    deleteMessages,
+    Widget,
+} from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
 import axios from "axios";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import './Messaging.css'
-import {fetchMessages} from "../../Redux/actions/actions-messaging";
+import {fetchPTsPatients} from "../../Redux/actions/actions-pt";
 
 const Messaging = (props) => {
-  useEffect(() => {
-      const data = {pt: props.pt.pt_id, patient: props.pt.selectedPatient.patient_id, email: props.pt.email}
-      props.fetchMessages(data);
-  }, [props.pt.selectedPatient.patient_id]);
+    useEffect(() => {
+        deleteMessages(1000);
+        fetchMessages();
+    }, [props.pt.selectedPatient.patient_id]);
 
-  const handleNewUserMessage = (newMessage) => {
-    const params = new URLSearchParams();
-    params.append("message", newMessage);
-    params.append("pt", props.pt.pt_id);
-    params.append("patient", props.pt.selectedPatient.patient_id);
+    const fetchMessages = () => {
+        axios
+            .get("api/pt/message/id", {
+                params: {
+                    pt: props.pt.pt_id,
+                    patient: props.pt.selectedPatient.patient_id,
+                },
+            })
+            .then((response) => {
+                response.data.map((message) => {
+                    console.log(message, props.pt)
+                    if (message.sender === props.pt.email) {
+                        addUserMessage(message.message);
+                    } else {
+                        addResponseMessage(message.message);
+                    }
+                });
+            })
+            .catch(err => console.log(err));
+    };
 
-    axios
-      .post("api/pt/message/register", params)
-      .then((response) => {
-        if (response.data == 200) {
-          console.log("Message success");
-        }
-      })
-      .catch(console.log);
-  };
+    const handleNewUserMessage = (newMessage) => {
+        const params = new URLSearchParams();
+        params.append("message", newMessage);
+        params.append("pt", props.pt.pt_id);
+        params.append("patient", props.pt.selectedPatient.patient_id);
 
-  return (
-    <div>
-        <Launcher
-            agentProfile={{
-                teamName: 'react-chat-window',
-                imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
-            }}
-            onMessageWasSent={handleNewUserMessage}
-            messageList={props.messages}
-            showEmoji={false}
-        />
-      {/*<Widget*/}
-      {/*  title={"Messages From"}*/}
-      {/*  subtitle={`${props.pt.selectedPatient.f_name} ${props.pt.selectedPatient.l_name}`}*/}
-      {/*  showTimeStamp={false}*/}
-      {/*  senderPlaceHolder={"Enter message..."}*/}
-      {/*  handleNewUserMessage={handleNewUserMessage}*/}
-      {/*/>*/}
-    </div>
-  );
+        axios
+            .post("api/pt/message/register", params)
+            .then((response) => {
+                if (response.data === 200) {
+                    console.log("Message success");
+                }
+            })
+            .catch(err => console.log(err));
+    };
+
+    return (
+        <div>
+            <Widget
+                title={props.pt.selectedPatient.patient_id ? "Messages From" : "Message Center"}
+                subtitle={props.pt.selectedPatient.patient_id ? `${props.pt.selectedPatient.f_name} ${props.pt.selectedPatient.l_name}` : "Choose a patient"}
+                showTimeStamp={false}
+                senderPlaceHolder={"Enter message..."}
+                handleNewUserMessage={handleNewUserMessage}
+            />
+        </div>
+    );
 };
 
 export default connect(
-  (state) => ({
-      pt: state.pt,
-      messages: state.messages.messages
-  }),
-  (dispatch) => ({
-      fetchMessages: (data) => dispatch(fetchMessages(data))
-  })
+    (state) => ({
+        pt: state.pt
+    }),
+    (dispatch) => ({
+        fetchPTsPatients: (pt_id) => dispatch(fetchPTsPatients(pt_id)),
+    })
 )(Messaging);
--- frontend/src/Redux/reducer/rootReducer.js
+++ frontend/src/Redux/reducer/rootReducer.js
@@ -2,13 +2,11 @@ import { combineReducers } from "redux";
 import PatientsReducer from "./reducer-patients";
 import PTReducer from "./reducer-pt";
 import WorkoutReducer from "./reducer-workouts";
-import MessagingReducer from "./reducer-messaging";
 
 const rootReducer = combineReducers({
   patients: PatientsReducer,
   pt: PTReducer,
-  exercises: WorkoutReducer,
-  messages: MessagingReducer
+  exercises: WorkoutReducer
 });
 
 export default rootReducer;
